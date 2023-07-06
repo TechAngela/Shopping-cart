2fdbae07d52d7439d558d0355351e633f3572bc9
Angelique Uwimbabazi <uwangela12@gmail.com>
2023-07-06 16:21:19 +0200

    able to update cart

--- Controllers/cartController.ts
+++ Controllers/cartController.ts
@@ -1,73 +1,105 @@
-import {Cart} from "../Models/cartModel";
-import {Product} from "../Models/productModel"
-const createCart = async(req:any,res:any):Promise<any>=>{
+import { Cart } from "../Models/cartModel";
+import { Product } from "../Models/productModel";
 
-     try{
-          const data=req.body
-          const found=await Product.findOne({name:data.name})
-          if(!found){
-              res.status(409).json({
-                  message:"this product is not available"
-              })
-          }else{
-              const amount=found.amount
-              const Quantity=found.Quantity
-              res.status(200).json({
-                  message:" for created cart",amount,Quantity
-              })
-  
-             const selected=new Cart({
-              name:found,
-              amount:amount,
-              Quantity:Quantity
-             })
-             selected.save()
+const createCart = async (req: any, res: any): Promise<any> => {
+
+     try {
+          const data = req.body
+          const found = await Product.findOne({ name: data.name })
+          if (!found) {
+               res.status(409).json({
+                    message: "this product is not available"
+               })
+          } else {
+               const name = found.name
+               const amount = found.amount
+               const Quantity = found.Quantity
+               res.status(200).json({
+                    message: " product added successfully on cart", name, amount, Quantity
+               })
+
+               const selected = new Cart({
+                    name: found.name,
+                    amount: amount,
+                    Quantity: Quantity
+               })
+               selected.save()
           }
-          
-      }catch(e){
-          console.log("the error occured while adding product to cart:",e)
-  
-      }    
-  
+
+     } catch (e) {
+          console.log("the error occured while adding product to cart:", e)
+
+     }
+
 }
 
-const viewCart=async(req:any,res:any): Promise<any>=>{
-     try{
-         const theCart=await Cart.find({})
-         console.log("all products in cart")
-         res.status(200).json({
-             message:"This is all product",theCart
-         })
- 
-     }catch(e){
-         console.log("THis is the error:",e)
-         res.status(400).json({
-             message:"error while viewing all carts",e
-         })
- 
-     }
- 
- }
- const deleteElement=async(req:any,res:any):Promise<any> =>{
-     try{
-         const data=req.body
-         const found=await Product.findOne({name:data.name})
-         if(!found){
-             res.status(409).json({
-                 message:"this products is not available"
-             })
-         }else{
-             const id=found._id
-             await Cart.deleteOne({_id:id})
-             res.status(200).json({
-                 message:"the product deleted successfully",
-             })
-         }
-     }catch(error){
-         console.log("error occured while deleting:",error)
- 
-     }
- }
+const viewCart = async (req: any, res: any): Promise<any> => {
+     try {
+          const theCart = await Cart.find({})
+          console.log("all products in cart")
+          res.status(200).json({
+               message: "This is all product", theCart
+          })
 
+     } catch (e) {
+          console.log("THis is the error:", e)
+          res.status(400).json({
+               message: "error while viewing all carts", e
+          })
 
-export{createCart , viewCart , deleteElement}
+     }
+
+}
+const deleteElement = async (req: any, res: any): Promise<any> => {
+     try {
+          const data = req.body
+          const found = await Cart.findOne({ name: data.name })
+          if (!found) {
+               res.status(409).json({
+                    message: "this products is not available", found
+               })
+          } else {
+               const id = found._id
+               await Cart.deleteOne({ _id: id })
+               res.status(200).json({
+                    message: "the product is deleted", id
+               })
+          }
+     } catch (e) {
+          console.log("error occured while deleting:", e)
+
+     }
+}
+
+const updateProduct = async (req: any, res: any): Promise<any> => {
+
+     try {
+          const data = req.body;
+          const found = await Cart.findOne({ name: data.name });
+          if (!found) {
+               res.status(409).json({
+                    message: "This product is not available",
+                    found,
+               });
+          } else {
+               const amount: any = found.amount;
+               const currentQuantity: any = found.Quantity;
+               const newQuantity = currentQuantity + 1;
+               const newTotalAmount = newQuantity * amount;
+               //     Cart.amount = newTotalAmount;
+               let updatedData = {
+                    ...data,
+                    Quantity: newQuantity,
+                    total_price: newTotalAmount
+               }
+               res.status(200).json({
+                    message: " data updated successfully on cart", updatedData
+               })
+               console.log("data updated", updatedData)
+          }
+     } catch (error) {
+          console.log("Error occurred while updating the cart", error);
+     }
+};
+
+export { createCart, viewCart, deleteElement, updateProduct }
--- Routes/cartRoute.ts
+++ Routes/cartRoute.ts
@@ -1,7 +1,8 @@
 import express from "express";
-import {createCart,viewCart, deleteElement } from "../Controllers/cartController"
+import {createCart,viewCart, deleteElement,updateProduct } from "../Controllers/cartController"
 const router =  express.Router();
 router.post("/addcart",createCart);
 router.get("/getCart",viewCart);
-router.delete("/deleteElement",deleteElement)
+router.delete("/deleteElement",deleteElement);
+router.put('/editQuantity', updateProduct)
 export {router};
