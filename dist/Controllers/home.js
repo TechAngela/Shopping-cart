a932289f11d839c528706ba8108574c30670852b
Angelique Uwimbabazi <uwangela12@gmail.com>
2023-07-07 16:09:54 +0200

    the deployment updates

--- dist/Controllers/home.js
+++ dist/Controllers/home.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const home = (req, res) => {
-    res.send("<h1>Hello</h1>"); // send response to client side
+    res.send("<h1>Shopping cart</h1>"); // send response to client side
 };
 exports.default = home;
 //# sourceMappingURL=home.js.map
